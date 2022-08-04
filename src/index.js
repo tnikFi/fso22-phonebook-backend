2f8c8e29f03caa4ffbe6dc19ab7955856cfc70a4
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-05 00:59:13 +0300

    Added request error handling

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const Contact = require('./models/Contact')
 
 morgan.token('body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -44,21 +44,42 @@ app.post('/api/persons', (req, res) => {
 	})
 })
 
-app.get('/api/persons/:id', (req, res) => {
-	Contact.findById(req.params.id).then(contact => {
-		if (contact) {
-			res.json(contact)
-		} else {
-			res.status(404).end()
-		}
-	})
+app.get('/api/persons/:id', (req, res, next) => {
+	Contact.findById(req.params.id)
+		.then(contact => {
+			if (contact) {
+				res.json(contact)
+			} else {
+				res.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id)
-	data.persons = data.persons.filter(person => person.id !== id)
-	res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+	Contact.findByIdAndRemove(req.params.id)
+		.then(result => {
+			res.status(204).end()
+		})
+		.catch(error => next(error))
 })
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+	next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
