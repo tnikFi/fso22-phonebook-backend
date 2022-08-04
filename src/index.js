5811c63ace2cdc67e1c1a7212df0cae824fc1df6
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-04 17:30:35 +0300

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 morgan.token('body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const generateId = () => Math.round(Math.random()*1e10)
